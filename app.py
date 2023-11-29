c9551b5c493f732e36e27235872917a249a91a4b
aFishboy <samuelhoward914@gmail.com>
2023-11-28 18:30:36 -0800

    azure

--- app.py
+++ app.py
@@ -47,4 +47,4 @@ def call_model():
         return jsonify(error='Invalid input. Please enter valid numbers.')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
